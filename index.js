9aa75cea20bd5bd96fede130842564745d08a58b
Alexis Delgado <alexisdelgado@Alexis-M1MBPro.localdomain>
2023-04-02 08:42:34 -0700

    finished assignment

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ function newImage (url, left, bottom){
     return object
     }
 
-
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+    
+    object.addEventListener('dblclick' ,function(){
+        object.remove()})
+    }
 
 // let pineTree = document.createElement('img')
 // pineTree.src = 'assets/pine-tree.png'
@@ -23,3 +28,8 @@ newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
